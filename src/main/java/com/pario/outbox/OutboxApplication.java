26e0e33cadc9b1a04b37088b97b0fde1b22ed83b
Temire.Emmanuel <temire.emmanuel@interswitchng.com>
2023-05-03 13:11:26 +0100

    Postgres Implementation ~2

--- src/main/java/com/pario/outbox/OutboxApplication.java
+++ src/main/java/com/pario/outbox/OutboxApplication.java
@@ -2,8 +2,10 @@ package com.pario.outbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
+@EnableJdbcRepositories(basePackages = "com.pario.outbox.repo")
 public class OutboxApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/pario/outbox/repo/OutboxEvents.java
+++ src/main/java/com/pario/outbox/repo/OutboxEvents.java
@@ -2,9 +2,11 @@ package com.pario.outbox.repo;
 
 import com.pario.outbox.model.OutboxEvent;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Temire.Emmanuel on 14/11/2022
  */
+@Repository
 public interface OutboxEvents extends CrudRepository<OutboxEvent, Long> {
 }
